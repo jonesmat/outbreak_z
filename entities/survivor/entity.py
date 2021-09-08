aec920e38a18866e83f54975e89c2de67d83f4f8
Matt Jones <matt.walker.jones@gmail.com>
2021-09-07 19:20:44 -0500

    Tweaks

--- entities/survivor/entity.py
+++ entities/survivor/entity.py
@@ -7,20 +7,20 @@ from entities.supplycrate.entity import SupplyCrate
 import entities.survivor.states as states
 
 
-
 class Survivor(GameEntity):
-    """ The survivor entity...  """
+    """The survivor entity..."""
+
     SIZE = 1  # meters wide and tall
     BASE_SPEED = 2  # meters/second
     SUPPLY_COST = 3
 
     def __init__(self, game, resource_mgr):
 
-        self.survivor_image = pygame.image.load('entities/survivor/survivor.png').convert_alpha()
-        self.survivor_dead_image = pygame.image.load('entities/survivor/survivor_dead.png').convert_alpha()
-        self.survivor_hit_image = pygame.image.load('entities/survivor/survivor_hit.png').convert_alpha()
+        self.survivor_image = pygame.image.load("entities/survivor/survivor.png").convert_alpha()
+        self.survivor_dead_image = pygame.image.load("entities/survivor/survivor_dead.png").convert_alpha()
+        self.survivor_hit_image = pygame.image.load("entities/survivor/survivor_hit.png").convert_alpha()
 
-        GameEntity.__init__(self, game, 'survivor', self.survivor_image, resource_mgr)
+        GameEntity.__init__(self, game, "survivor", self.survivor_image, resource_mgr)
 
         self.size = Survivor.SIZE
 
@@ -46,29 +46,28 @@ class Survivor(GameEntity):
         self.evade_until = None
 
     def bitten(self):
-        """ Damages the survivor and checks for death. """
+        """Damages the survivor and checks for death."""
         self.health -= 1
         self.image = self.survivor_hit_image
         self.was_hit = True
         if self.health <= 0:
-            self.brain.set_state('dead')
+            self.brain.set_state("dead")
 
     def draw(self, surface):
-        """ Handles drawing of the entity """
+        """Handles drawing of the entity"""
         # Call the draw function of the base class
         GameEntity.draw(self, surface)
 
-        # Update survivor image to his alive image if he has restored health above 0.
+        # Convert the "viewport" coordinates into "Device" coordinates for drawing
+        dev_location = self.game.scene.get_dev_vec_from_viewport_vec(self.location)
+
+        # Update survivor image to his alive image if they have recovered.
         if self.was_hit and self.health > 0:
             self.image = self.survivor_image
             self.was_hit = False
 
-        # Draw caution icon above survivor if he's out of ammo.
         if self.ammo < 1 and self.health > 0:
-            dev_location = self.game.scene.get_dev_vec_from_viewport_vec(self.location)
-
-            width, height = self.resource_mgr.caution_image.get_size()
-            surface.blit(self.resource_mgr.caution_image, (dev_location.x - width / 2, (dev_location.y - height / 2) - 10))
+            self._draw_caution_above_survivor(surface)
 
         # Debug drawing of target zombie line.
         if self.debug_mode:
@@ -81,5 +80,12 @@ class Survivor(GameEntity):
             surface.blit(self.resource_mgr.font.render(str(self.ammo), True, (0, 0, 0)), dev_location - Vector2(20, 0))
 
             # blit health
-            surface.blit(self.resource_mgr.font.render(str(self.health), True, (0, 0, 0)), dev_location - Vector2(5, 22))
+            surface.blit(
+                self.resource_mgr.font.render(str(self.health), True, (0, 0, 0)), dev_location - Vector2(5, 22)
+            )
 
+    def _draw_caution_above_survivor(self, surface):
+        dev_location = self.game.scene.get_dev_vec_from_viewport_vec(self.location)
+
+        width, height = self.resource_mgr.caution_image.get_size()
+        surface.blit(self.resource_mgr.caution_image, (dev_location.x - width / 2, (dev_location.y - height / 2) - 10))
